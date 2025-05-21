3b97f0a5a2a2fa8291a6390559e5ccf70e68d88b
송혁규 <goldenbeard92@gmail.com>
2025-05-21 21:50:23 +0900

    05210950

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -610,11 +610,10 @@ return (
         <div dangerouslySetInnerHTML={{
           __html: `
             <ins class="kakao_ad_area" style="display:none;"
-              data-ad-unit="DAN-GhVfyT9ccl0nsDOW"
-              data-ad-width="320"
-              data-ad-height="100">
-            </ins>
-            <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+data-ad-unit = "DAN-b68sSnUxw2CfTs04"
+data-ad-width = "320"
+data-ad-height = "100"></ins>
+<script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
           `
         }} />
       </div>
